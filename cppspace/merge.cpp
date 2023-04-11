b70ca15f01a51dd38edb9f7109ce0afc7d228771
Swaylivrx <1617003785@qq.com>
2023-04-11 15:19:22 +0800

    八股文内容笔记

---
+++ cppspace/merge.cpp
@@ -0,0 +1,13 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, const char** argv) {
+	vector<int> res = {2321,2,1,4,6};
+	sort(res.begin(), res.end());
+	for(auto var : res)
+	{
+		cout << var << endl;
+	}
+	return 0;
+}
---
+++ javaspace/src/main/java/com/aoeivux/Demo1.java
@@ -0,0 +1,17 @@
+package com.aoeivux;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		final StringBuilder sb = new StringBuilder("sb");
+		sb.append('a');
+		System.out.println(sb);
+	}
+}
+
+class Person {
+	private String name = "asd";
+	private String last = "adsf";
+
+	public static void main(String[] args) {
+	}
+}
